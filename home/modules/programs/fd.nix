9b431581449e4296d5c0e8cbe0874bab8599c1ed
june <me@koi.rip>
2026-05-14 11:25:01 +0200

    fd: add more ignores

--- home/modules/programs/fd.nix
+++ home/modules/programs/fd.nix
@@ -2,6 +2,17 @@
   programs.fd = {
     enable = true;
     hidden = true;
-    ignores = [ ".git" ];
+    ignores = [
+      ".git"
+      ".jj"
+      ".zig-cache"
+      "zig-out"
+      "target"
+      "result"
+      "bin"
+      "build"
+      "dist"
+      "node_modules"
+    ];
   };
 }
