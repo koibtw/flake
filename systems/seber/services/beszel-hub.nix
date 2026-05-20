f26b9e412acc0fd42f9f36bf98bb0816ba223251
june <me@koi.rip>
2026-05-21 00:36:51 +0200

    seber: use CLI for beszel tailscale serve

--- systems/seber/services/beszel-hub.nix
+++ systems/seber/services/beszel-hub.nix
@@ -11,9 +11,4 @@
     host = "127.0.0.1";
     environmentFile = config.age.secrets.beszel-env.path;
   };
-
-  services.tailscale.serve = {
-    enable = true;
-    services.beszel.endpoints."tcp:443" = "http://127.0.0.1:6767";
-  };
 }
