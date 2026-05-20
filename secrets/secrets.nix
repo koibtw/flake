07eb81f3a1f7c8c768c4e9b77c5273f7b89be3f1
june <me@koi.rip>
2026-05-20 23:21:00 +0200

    secrets: reorganize

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,21 +1,21 @@
 let
-  koi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9feAlrIVPuVS28tz54/m7pZkjHHL5aVhV2qwBPPceT";
-  seber = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAp82e57ij5x+Ei1Tjwxuex0fil5vV9wfEfNYgC5dFQ";
+  koi = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9feAlrIVPuVS28tz54/m7pZkjHHL5aVhV2qwBPPceT" ];
+  seber = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAp82e57ij5x+Ei1Tjwxuex0fil5vV9wfEfNYgC5dFQ" ];
 in
 {
-  "cloudflared.pem.age".publicKeys = [ koi ];
-  "jellyfin-rpc.json.age".publicKeys = [ koi ];
-  "tranquil.env.age".publicKeys = [ koi ];
+  "cloudflared.pem.age".publicKeys = koi;
+  "jellyfin-rpc.json.age".publicKeys = koi;
+  "tranquil.env.age".publicKeys = koi;
 
-  "email-pass-me.age".publicKeys = [ koi ];
-  "email-pass-noreply.age".publicKeys = [ koi ];
+  "email-pass-me.age".publicKeys = koi;
+  "email-pass-noreply.age".publicKeys = koi;
 
-  "tailscale-auth.age".publicKeys = [ koi seber ];
+  "tailscale-auth.age".publicKeys = koi ++ seber;
 
-  "beszel.env.age".publicKeys = [ seber ];
-  "vaultwarden.env.age".publicKeys = [ seber ];
-  "ssl-koi.cert.pem.age".publicKeys = [ seber ];
-  "ssl-koi.key.pem.age".publicKeys = [ seber ];
-  "ssl-ebil.cert.pem.age".publicKeys = [ seber ];
-  "ssl-ebil.key.pem.age".publicKeys = [ seber ];
+  "beszel.env.age".publicKeys = seber;
+  "vaultwarden.env.age".publicKeys = seber;
+  "ssl-koi.cert.pem.age".publicKeys = seber;
+  "ssl-koi.key.pem.age".publicKeys = seber;
+  "ssl-ebil.cert.pem.age".publicKeys = seber;
+  "ssl-ebil.key.pem.age".publicKeys = seber;
 }
