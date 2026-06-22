bc7deef132b6fc3b914ef67f310eef27f5d92850
june <me@koi.rip>
2026-06-22 13:18:00 +0200

    halloy: register oftc

--- home/programs/graphical/halloy.nix
+++ home/programs/graphical/halloy.nix
@@ -1,6 +1,17 @@
 { config, ... }:
+let
+  metadata = {
+    realname = "june";
+    metadata = {
+      color = "#cbe3b3";
+      display-name = "june";
+      homepage = "https://koi.rip";
+      pronouns = "they/them";
+    };
+  };
+in
 {
-  age.secrets.libera-password.file = ../../../secrets/libera-password.age;
+  age.secrets.irc-password.file = ../../../secrets/irc-password.age;
   programs.halloy = {
     enable = true;
     settings = {
@@ -15,24 +26,31 @@
           "pronouns"
         ];
       };
-      servers.liberachat = {
-        server = "irc.libera.chat";
-        nickname = "junekot";
-        realname = "june";
-        metadata = {
-          color = "#cbe3b3";
-          display-name = "june";
-          homepage = "https://koi.rip";
-          pronouns = "they/them";
-        };
-        queries = [ "robinwobin" ];
-        monitor = [ "robinwobin" ];
-        channels = [ "#neovim" ];
-      };
-      servers.liberachat.sasl.plain = {
-        username = "junekot";
-        password_command = "cat ${config.age.secrets.libera-password.path}";
-        disconnect_on_failure = true;
+      servers = {
+        liberachat = {
+          server = "irc.libera.chat";
+          nickname = "junekot";
+          queries = [ "robinwobin" ];
+          monitor = [ "robinwobin" ];
+          channels = [ "#neovim" ];
+          sasl.plain = {
+            username = "junekot";
+            password_command = "cat ${config.age.secrets.irc-password.path}";
+            disconnect_on_failure = true;
+          };
+        }
+        // metadata;
+        oftc = {
+          server = "irc.oftc.net";
+          nickname = "june";
+          nick_password_command = "cat ${config.age.secrets.irc-password.path}";
+          channels = [
+            "#alpine-linux"
+            "#alpine-devel"
+            "#alpine-offtopic"
+          ];
+        }
+        // metadata;
       };
     };
   };
