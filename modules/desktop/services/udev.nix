d06d3f1e87c861de4ff434433e37cad4ed08b771
robin <comfysagedev@gmail.com>
2026-07-14 19:09:33 +0200

    desktop: add qmk udev rules

--- modules/desktop/services/udev.nix
+++ modules/desktop/services/udev.nix
@@ -1,6 +1,10 @@
+{ pkgs, ... }:
 {
-  services.udev.extraRules = ''
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="02a0", MODE="0660", TAG+="uaccess"
-    ATTRS{name}=="*DualSense*Touchpad*", ENV{ID_INPUT_TOUCHPAD}=""
-  '';
+  services.udev = {
+    packages = [ pkgs.qmk-udev-rules ];
+    extraRules = ''
+      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="02a0", MODE="0660", TAG+="uaccess"
+      ATTRS{name}=="*DualSense*Touchpad*", ENV{ID_INPUT_TOUCHPAD}=""
+    '';
+  };
 }
