22f932671ff318d13bdc06089bffeea47467861b
june <me@koi.rip>
2026-07-16 21:57:59 +0200

    seber: fix ipv6

--- systems/seber/default.nix
+++ systems/seber/default.nix
@@ -20,6 +20,19 @@
       80
       443
     ];
+
+    networkmanager.ensureProfiles.profiles."Wired connection 3" = {
+      connection = {
+        id = "Wired connection 3";
+        type = "ethernet";
+        interface-name = "ens5";
+      };
+      ipv6 = {
+        method = "auto";
+        addr-gen-mode = "eui64";
+        ip6-privacy = 0;
+      };
+    };
   };
 
   environment.systemPackages = [
